fe99c9ebcd395118b73cbb113a3478022ec48bee
augustin grosnon <augustin.grosnon@epitech.eu>
2024-08-09 23:37:57 +0200

    update: use sdf for everything

--- scripts/shaderModule.js
+++ scripts/shaderModule.js
@@ -16,7 +16,7 @@ export const shaderCode = `
   };
 
   @group(0) @binding(0) var<uniform> circleParams: vec2f;
-  @group(0) @binding(1) var<uniform> lines: array<vec4f, 5>; // ? is there any way to avoid fixed-size arrays?
+  @group(0) @binding(1) var<uniform> lines: array<vec4f, 5>;
   @group(0) @binding(2) var<uniform> colors: ColorUniforms;
 
   fn getBackgroundGradient(pos: vec2f) -> vec4f {
@@ -28,15 +28,6 @@ export const shaderCode = `
     return mix(colors.circleBottomColor, colors.circleTopColor, circleY);
   }
 
-  fn isPixelInLines(pixelY: f32, lines: array<vec4f, 5>) -> bool {
-    for (var i = 0u; i < 5u; i++) {
-      if ((pixelY >= lines[i].x && pixelY <= lines[i].y) || (pixelY <= lines[i].x && pixelY >= lines[i].y)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   @vertex
   fn vertexMain(input: VertexInput) -> VertexOutput {
     var output: VertexOutput;
@@ -45,19 +36,50 @@ export const shaderCode = `
     return output;
   }
 
+  fn sdUnion(d1: f32, d2: f32) -> f32 {
+    return min(d1, d2);
+  }
+
+  fn sdSubstract(d1: f32, d2: f32) -> f32 {
+    return max(d1, -d2);
+  }
+
+  fn sdIntersect(d1: f32, d2: f32) -> f32 {
+    return max(d1, d2);
+  }
+
+  fn sdCircle(pos: vec2f, center: vec2f, radius: f32) -> f32 {
+    return length(pos - center) - radius;
+  }
+
+  fn sdRectangle(pos: vec2f, center: vec2f, size: vec2f) -> f32 {
+    let d = abs(pos - center) - size * 0.5;
+    return length(max(d, vec2f(0.0, 0.0))) + min(max(d.x, d.y), 0.0);
+  }
+
+  fn sdVerticalLine(pos: vec2f, lineStart: f32, lineEnd: f32, circleRadius: f32) -> f32 {
+    let rectCenter = vec2f(0.0, (lineStart + lineEnd) * 0.5);
+    let rectSize = vec2f(circleRadius * 2.0, abs(lineStart - lineEnd));
+    // ! the previous two should be done only one time at start, not everytime
+    return sdRectangle(pos, rectCenter, rectSize);
+  }
+
   @fragment
   fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
     let circleCenter = vec2f(0.0, 0.0);
     let circleRadius = circleParams.x;
 
-    let dist = length(input.fragPos - circleCenter) - circleRadius;
+    var sdf = sdCircle(input.fragPos, circleCenter, circleRadius);
 
-    if (dist >= 0.0 || isPixelInLines(input.fragPos.y, lines)) {
-      return getBackgroundGradient(input.fragPos);
-    } else {
+    for (var i = 0u; i < 5u; i++) {
+      let lineSdf = sdVerticalLine(input.fragPos, lines[i].x, lines[i].y, circleRadius);
+      sdf = sdSubstract(sdf, lineSdf);
+    }
+
+    if (sdf < 0.0) {
       return getCircleGradient(input.fragPos, circleCenter, circleRadius);
+    } else {
+      return getBackgroundGradient(input.fragPos);
     }
   }
 `;
-
-// ? add border around elems?
